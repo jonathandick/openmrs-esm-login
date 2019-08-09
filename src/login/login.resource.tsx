02dbb382af57f3fa7432ba4b4b0e02f76baaa522
Jonathan Dick <jonathandick@Jonathans-MacBook-Pro-2.local>
2019-08-09 08:54:30 -0700

    Updated to use window.openmrsBase

--- src/login/login.resource.tsx
+++ src/login/login.resource.tsx
@@ -1,20 +1,23 @@
 export function performLogin(username, password) {
   const token = window.btoa(`${username}:${password}`);
-  return window
-    .fetch(`/openmrs/ws/rest/v1/session`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: `Basic ${token}`
-      }
-    })
-    .then(resp => {
-      if (resp.ok) {
-        return resp.json();
-      } else {
-        throw Error(
-          `Could not login. Server responded with status ${resp.status}`
-        );
-      }
-    });
+  return (
+    window
+      // @ts-ignore
+      .fetch(window.openmrsBase + `/openmrs/ws/rest/v1/session`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Basic ${token}`
+        }
+      })
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          throw Error(
+            `Could not login. Server responded with status ${resp.status}`
+          );
+        }
+      })
+  );
 }
